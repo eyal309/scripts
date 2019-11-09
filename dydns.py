4885b7970a79d61c75e05cf6742d831c343245e2
Eyal <eyal309@gmail.com>
2019-11-09 21:52:20 +0200

    Update dydns.py

--- dydns.py
+++ dydns.py
@@ -14,6 +14,8 @@ auth_key = '<auth key>' # add your auth key, you need to generate it
 zone_id = '<zone id>' # add your zone ID, you can find it in your account overview
 record_id = '<dns record id>' # add your dns record ID, you can find it using check_ip_addr func
 
+api_url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/'
+
 headers = {
     'X-Auth-Email': '<account email>',
     'X-Auth-Key': auth_key,
@@ -25,13 +27,10 @@ def edit_dns_record():
     record_data = {"type": "A", "name": "@", "content": ext_ip, "ttl": 1, "proxied": True}
     data = json.dumps(record_data)
 
-    response = json.loads(
-        requests.put(f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}', headers=headers,
-                     data=data).text)
+    response = json.loads(requests.put(f'{api_url}{record_id}', headers=headers, data=data).text)
     if response['success']:
         return True
-    else:
-        return response
+    email_alert(f'error: {response}')
 
 
 def check_ip_addr():
@@ -41,10 +40,18 @@ def check_ip_addr():
         ('page', '1')
     )
 
-    response = json.loads(
-        requests.get(f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records', headers=headers,
-                     params=params).text)
+    response = json.loads(requests.get(api_url, headers=headers, params=params).text)
     return response['result'][0]['content']
+
+
+def email_alert(msg):
+    fromaddr = '<from address>' # edit here
+    toaddrs = '<to address>' # edit here
+    server = SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login('<your email>', '<your password>') # edit here
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
 
 
 if not check_ip_addr() == ext_ip:
